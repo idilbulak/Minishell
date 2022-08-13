da5f4a4aa2d6d3a2dabb61ae5e97968be621119a
Daniel Saat <dsaat@f1r1s9.codam.nl>
2022-08-13 16:01:21 +0200

    cleanup

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ibulak <ibulak@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/06 10:25:01 by ibulak        #+#    #+#                 */
-/*   Updated: 2022/08/13 15:40:40 by dsaat         ########   odam.nl         */
+/*   Updated: 2022/08/13 16:01:06 by dsaat         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,9 @@ int	main(int argc, char **argv, char **environ)
 	t_word_list	*word_list;
 	t_symtab	**symtab;
 
-	// int i = 0;
-	// while (environ[i])
-	// 	printf("%s\n", environ[i++]);
 	if (argc == 1 && ft_strncmp("minishell", argv[0], 8))
 	{
 		symtab = init_env_symtab(environ);
-		// system("leaks minishell");
 		init_signals();
 		while (1)
 		{
