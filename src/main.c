75a3df950a9d41a41f557c07a34187bf11a9623c
Idil Bulak <ibulak@f1r1s1.codam.nl>
2022-08-30 13:27:25 +0200

    termios structure

--- src/main.c
+++ src/main.c
@@ -6,13 +6,32 @@
 /*   By: ibulak <ibulak@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/06 10:25:01 by ibulak        #+#    #+#                 */
-/*   Updated: 2022/08/29 16:33:24 by dsaat         ########   odam.nl         */
+/*   Updated: 2022/08/30 13:25:23 by ibulak        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+#include <termios.h>
 
-static void	init_minishell(char **environ)
+char	*ft_readline(struct termios original_tcattr)
+{
+	char			*str;
+	struct termios	new_tcattr;
+
+	tcgetattr(STDIN_FILENO, &original_tcattr);
+	tcgetattr(STDIN_FILENO, &new_tcattr);
+	new_tcattr.c_lflag &= ~ECHOCTL;
+	tcsetattr(STDIN_FILENO, TCSANOW, &new_tcattr);
+	register_signals();
+	str = readline("minishell$ ");
+	if (!str)
+		exit(EXIT_FAILURE);
+	tcsetattr(STDIN_FILENO, TCSANOW, &original_tcattr);
+	unregister_signals();
+	return (str);
+}
+
+static void	init_minishell(char **environ, struct termios original_tcattr)
 {
 	char		*str;
 	t_token		*tokens;
@@ -20,10 +39,10 @@ static void	init_minishell(char **environ)
 	t_symtab	**symtab;
 
 	symtab = init_env_symtab(environ);
-	init_signals();
+	register_signals();
 	while (1)
 	{
-		str = readline("minishell$  ");
+		str = ft_readline(original_tcattr);
 		if (!str)
 			exit(EXIT_FAILURE);
 		add_history(str);
@@ -39,8 +58,11 @@ static void	init_minishell(char **environ)
 
 int	main(int argc, char **argv, char **environ)
 {
+	struct termios	original_tcattr;
+
 	(void)argv;
+	tcgetattr(STDIN_FILENO, &original_tcattr);
 	if (argc == 1)
-		init_minishell(environ);
+		init_minishell(environ, original_tcattr);
 	return (0);
 }
